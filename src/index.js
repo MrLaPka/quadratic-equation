7874c1c87fc5860e156fddfa03bd5d9e8eb86057
Егор <42974925+MrLaPka@users.noreply.github.com>
2018-09-22 22:53:43 +0300

    4th task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  let D;
+  let newstring = equation.split(" ");
+  let numbers = new Array;
+  let increment_for_numbers = 0;
+  let increment_for_operators = 0;
+  let xmass = new Array;
+  let operators = new Array;
+  for(let i = 0; i<newstring.length;i++){
+    if(parseInt(newstring[i])){
+    numbers[increment_for_numbers] = Math.round(parseInt(newstring[i]));
+    increment_for_numbers++;
+    }
+    if((newstring[i] == '+')||(newstring[i] == '-')||(newstring[i] == '*')){
+     operators[increment_for_operators] = newstring[i];
+     increment_for_operators++;
+     }// your implementation
+}
+if(operators[3] == '+')
+D = numbers[1]*numbers[1] - 4*numbers[0]*numbers[2];
+else if(operators[3] == '-')
+D = numbers[1]*numbers[1] + 4*numbers[0]*numbers[2];
+if(operators[1] == '+'){
+xmass[0] = Math.round(((-1*numbers[1])+Math.sqrt(D))/(2*numbers[0]));
+xmass[1] = Math.round(((-1*numbers[1])-Math.sqrt(D))/(2*numbers[0]));
+}
+else if(operators[1] == '-'){
+xmass[0] = Math.round((numbers[1]+Math.sqrt(D))/(2*numbers[0]));
+xmass[1] = Math.round((numbers[1]-Math.sqrt(D))/(2*numbers[0]));
+}
+if(xmass[0]>xmass[1]){
+  let temp = xmass[0];
+  xmass[0] =xmass[1];
+  xmass[1] = temp;
+}
+return xmass;
 }
